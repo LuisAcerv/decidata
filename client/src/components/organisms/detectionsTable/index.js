6c4c51079d5b13ad6220e76fb6cd4090c20071ae
LuisCervantes <l.arr.cerv@gmail.com>
2019-07-20 16:48:15 -0500

    Remove useless import

--- client/src/components/organisms/detectionsTable/index.js
+++ client/src/components/organisms/detectionsTable/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Table } from "antd";
 import DetectionsContext from "../../../context/context";
-import moment from "moment";
 
 const styles = {
   table: { margin: "0 auto", width: `${window.innerWidth - 100}px` },
