30cf46209c77e39dfdb214ceeff26bec161fa841
LuisCervantes <l.arr.cerv@gmail.com>
2019-07-20 09:38:06 -0500

    update home page with new components chart and detections table

--- client/src/components/pages/home/index.js
+++ client/src/components/pages/home/index.js
@@ -1,4 +1,11 @@
 import React from "react";
+import { CardHolder, Chart, DetectionsTable } from "../../organisms";
 import { Dashboard } from "../../templates";
 
-export default () => <Dashboard />;
+export default () => (
+  <Dashboard>
+    <CardHolder />
+    <Chart />
+    <DetectionsTable />
+  </Dashboard>
+);
