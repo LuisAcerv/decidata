b08f41b56f735ec107acaa9c79be23553c58a857
LuisCervantes <l.arr.cerv@gmail.com>
2019-07-20 10:41:17 -0500

    remove mocked data and pass context

--- client/src/components/organisms/detectionsTable/index.js
+++ client/src/components/organisms/detectionsTable/index.js
@@ -1,41 +1,7 @@
 import React from "react";
 import { Table } from "antd";
+import DetectionsContext from "../../../context/context";
 import moment from "moment";
-
-const dataSource = [
-  {
-    key: "1",
-    date: moment().format("DD/MM/YYYY"),
-    channel: "Fox",
-    brand: "Coca cola",
-    commercial: "Coca cola zero",
-  },
-];
-
-const columns = [
-  {
-    title: "Date",
-    dataIndex: "date",
-    key: "date",
-    sorter: (a, b) => a.name.length - b.name.length,
-    sortDirections: ["descend"],
-  },
-  {
-    title: "Channel",
-    dataIndex: "channel",
-    key: "channel",
-  },
-  {
-    title: "Brand",
-    dataIndex: "brand",
-    key: "brand",
-  },
-  {
-    title: "Commercial",
-    dataIndex: "commercial",
-    key: "commercial",
-  },
-];
 
 const styles = {
   table: { margin: "0 auto", width: `${window.innerWidth - 100}px` },
@@ -45,8 +11,16 @@ const { table } = styles;
 
 export default props => {
   return (
-    <div style={table}>
-      <Table dataSource={dataSource} columns={columns} />
-    </div>
+    <DetectionsContext.Consumer>
+      {context => (
+        <div data={context} style={table}>
+          <p>Detections</p>
+          <Table
+            dataSource={context.detections.dataSource}
+            columns={context.detections.columns}
+          />
+        </div>
+      )}
+    </DetectionsContext.Consumer>
   );
 };
