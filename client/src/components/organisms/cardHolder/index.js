7ba42fb5745937d6c58b498b4fef606536c4c5c8
LuisCervantes <l.arr.cerv@gmail.com>
2019-07-20 16:49:50 -0500

    Implement context

--- client/src/components/organisms/cardHolder/index.js
+++ client/src/components/organisms/cardHolder/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { Col, Row } from "antd";
+import { Col, Row, Icon, Statistic } from "antd";
+import moment from "moment";
 import { Card } from "../../ molecules";
+import DetectionsContext from "../../../context/context";
 
-export default props => {
+export default () => {
   const styles = {
     container: {
       background: "#FFFFFF",
@@ -13,24 +15,60 @@ export default props => {
   const { container } = styles;
 
   return (
-    <div style={container}>
-      <Row gutter={16}>
-        <Col span={6}>
-          <Card title="Channel with most detections" content={"Card content"} />
-        </Col>
-        <Col span={6}>
-          <Card title="Brand with most detections" content={"Card content"} />
-        </Col>
-        <Col span={6}>
-          <Card
-            title="Commercial with most detections"
-            content={"Card content"}
-          />
-        </Col>
-        <Col span={6}>
-          <Card title="Day with most detections" content={"Card content"} />
-        </Col>
-      </Row>
-    </div>
+    <DetectionsContext.Consumer>
+      {context => (
+        <div style={container}>
+          <Row gutter={16}>
+            <Col span={6}>
+              <Card
+                title="Channel with most detections"
+                content={
+                  <Statistic
+                    title={context.channelWithMoreDetections.name}
+                    value={context.channelWithMoreDetections.count}
+                  />
+                }
+              />
+            </Col>
+            <Col span={6}>
+              <Card
+                title="Brand with most detections"
+                content={
+                  <Statistic
+                    title={context.brandWithMoreDetections.name}
+                    value={context.brandWithMoreDetections.count}
+                  />
+                }
+              />
+            </Col>
+            <Col span={6}>
+              <Card
+                title="Commercial with most detections"
+                content={
+                  <Statistic
+                    title={context.commercialWithMoreDetections.name}
+                    value={context.commercialWithMoreDetections.count}
+                  />
+                }
+              />
+            </Col>
+            <Col span={6}>
+              <Card
+                title="Day with most detections"
+                content={
+                  <Statistic
+                    title={moment(
+                      context.dateWithMoreDetections.name,
+                      "YYYY-MM-DD HH:mm:ss"
+                    ).format("dddd")}
+                    value={context.dateWithMoreDetections.count}
+                  />
+                }
+              />
+            </Col>
+          </Row>
+        </div>
+      )}
+    </DetectionsContext.Consumer>
   );
 };
