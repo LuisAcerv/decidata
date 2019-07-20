c16346fe3c339ff21ef181f9c74ed2592ea6bb8a
LuisCervantes <l.arr.cerv@gmail.com>
2019-07-20 16:49:32 -0500

    Implement context

--- client/src/components/organisms/chart/index.js
+++ client/src/components/organisms/chart/index.js
@@ -24,24 +24,27 @@ const { container, chart } = styles;
 
 export default () => {
   return (
-    <div style={container}>
+    <DetectionsContext.Consumer>
+      {context => (
+        <div style={container}>
           <p>
             <b>Number of detections per day</b>
           </p>
-      <BarChart
-        width={window.innerWidth - 100}
-        height={200}
-        data={data}
-        margin={chart}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="pv" fill="#8884d8" />
-        <Bar dataKey="uv" fill="#82ca9d" />
-      </BarChart>
-    </div>
+          <BarChart
+            width={window.innerWidth - 100}
+            height={200}
+            data={context.chartData}
+            margin={chart}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="detections" fill="#8884d8" />
+          </BarChart>
+        </div>
+      )}
+    </DetectionsContext.Consumer>
   );
 };
