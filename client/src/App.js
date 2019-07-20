9be07d33e961c75840b5874e95a60d9b84644adb
LuisCervantes <l.arr.cerv@gmail.com>
2019-07-20 10:41:27 -0500

    wrap home page with context

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { Home } from "./components/pages";
+import DetectionsProvider from "./context/provider";
 export default () => {
-  return <Home />;
+  return (
+    <DetectionsProvider>
+      <Home />
+    </DetectionsProvider>
+  );
 };
